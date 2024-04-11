869c55c7830d259ee7d23af1afb6ed2b6bbcd41e
Luis Andrey Zuñiga Hernández <luisandreyzuniga@gmail.com>
2024-04-10 23:47:49 -0600

    Changing the error message return from a plain string to a json

--- recommendation_cloud_func/main.py
+++ recommendation_cloud_func/main.py
@@ -6,23 +6,38 @@ import json
 
 @functions_framework.errorhandler(AssertionError) #to check Post method
 def handle_assertion_error(e):
-    return "Method not allowed", 405
+    body={
+        "msg":"Method not allowed"
+    }
+    return body, 405
 
 @functions_framework.errorhandler(KeyError) #"Check json input structure. The json should have a meal object a a recommmendation_of's list of string"
 def handle_key_error(e):
-    return "Bad request", 400
+    body={
+        "msg": "Bad request the json must hava a meal and a recommendation of keys. The meal must have the fields: main_dish,drink and dessert"
+    }
+    return body, 400
 
 @functions_framework.errorhandler(ValidationError) #"Checks the meal object type and the type of recommedation of"
 def handle_validation_error(e):
-    return "Bad request", 400
+    body={
+        "msg":"Bad request the wrong type for either meal for recommendation_of"
+    }
+    return body , 400
 
 @functions_framework.errorhandler(ValueError) #"If the main_dish/drink/dessert or type of item does not exist in the database"
 def handle_value_error(e):
-    return str(e), 400
+    body={
+        "msg":str(e)
+    }
+    return body, 400
 
 @functions_framework.errorhandler(TypeError) #"If the main_dish/drink/dessert or type of item does not exist in the database"
 def handle_type_error(e):
-    return str("meal must be a object"), 400
+    body= {
+        "msg": "The item selected its not in the database"
+    }
+    return body, 400
 
 @functions_framework.http
 def recommendations(request):
@@ -40,6 +55,6 @@ def process_input(request_json):
     meal = Meal(main_dish=meal_json["main_dish"], drink=meal_json["drink"],dessert=meal_json["dessert"])
     recommedation_of=request_json['recommendation_of']
     if(type(recommedation_of) != list):
-        raise KeyError()
+        raise ValidationError()
     return meal,recommedation_of
 
